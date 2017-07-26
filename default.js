c3045a14ba18a0b97f28e98b1d63da45181eb92f
Macbook Air <macbookair@Macbooks-MacBook-Air.local>
2017-07-26 12:18:55 +1000

    changed name of function from "server" to "createForm" removed the 404 catch as no error will ever occur in the demo

--- default.js
+++ default.js
@@ -9,7 +9,7 @@ var filename = config.homepage;								//the default page is chosen from the con
 http.createServer(function (req, res) {
 	switch (req.method) {
 		case "GET":
-			server(req, res);								//The Node server will create a webpage using the server function
+			createForm(res);								//The Node server will create a webpage using the server function
 		break;
 		case "POST":
 			paymentService.acceptPost(req, res); 			//The Node server will receive a message using the receiver module
@@ -18,12 +18,8 @@ http.createServer(function (req, res) {
 }).listen(serverport);
 console.log("server running on port " + serverport);		//the server is opened, ready for use
 
-function server(req, res){
+function createForm(res){
 	fs.readFile(filename, (err, data) => {
-		if (err) {											//If the specified page isn't available, an error is created
-			res.writeHead(404, {'Content-Type': 'text/html'});
-			return res.end("404 Not Found");
-		} 
 		res.end(data);										//The page is given to the user
 	});
 }
