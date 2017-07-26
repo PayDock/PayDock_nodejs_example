e749f9d4f03f2973fd02d0ec602ab98ba3ec8baf
Macbook Air <macbookair@Macbooks-MacBook-Air.local>
2017-07-26 11:46:00 +1000

    merged all widget configuartions into one function, removing all the nasty regex

--- createWidget.js
+++ createWidget.js
@@ -1,20 +1,22 @@
 var config = require('./config.json');
 
-function example(res, div_num){
-	if (config.gateway_config[div_num].gateway_id.length == 24) {
-		res.writeHead(200, {
-			'Content-Type': 'text/javascript'
-		});
-		res.write("var widget = new paydock.HtmlWidget('#widget" + div_num + "', '" + config.PublicKey + "', '" + config.gateway_config[div_num].gateway_id + "', '" + config.gateway_config[div_num].payment_type + "');");
-		for (i = 0; i < config.gateway_config[div_num].form_fields.length; i++) {
-			res.write("\nwidget.setFormFields(['" + config.gateway_config[div_num].form_fields[i] + "']);");
+function example(res){
+	res.writeHead(200, {
+		'Content-Type': 'text/javascript'
+	});
+	for (foo = 0; foo < config.gateway_config.length; foo++) {
+		var div_id = foo + 1;
+		if (config.gateway_config[foo].gateway_id.length == 24) {
+			res.write("var widget" + foo + " = new paydock.HtmlWidget('#widget" + div_id + "', '" + config.PublicKey + "', '" + config.gateway_config[foo].gateway_id + "', '" + config.gateway_config[foo].payment_type + "');\n");
+			for (i = 0; i < config.gateway_config[foo].form_fields.length; i++) {
+				res.write("widget" + foo + ".setFormFields(['" + config.gateway_config[foo].form_fields[i] + "']);\n");
+			}
+			res.write("widget" + foo + ".onFinishInsert('[name=\"ps\"]', 'payment_source'); \nwidget" + foo + ".load();\n");
+		} else {
+			res.write("//no valid gateway id found for payment source\n" + foo);
 		}
-		res.write("\nwidget.onFinishInsert('[name=\"ps\"]', 'payment_source'); \nwidget.load();");
-		res.end();
-	} else {
-		res.write("//no gateway id found");
-		res.end();
 	}
+	res.end();
 }
 exports.example = example;
 
--- server.js
+++ server.js
@@ -6,31 +6,25 @@ var createWidget = require('./createWidget.js');
 function createPage(req, res){
 	var q = url.parse(req.url, true);
 	var filename = q.pathname;
-	var regextest = "";
-	
-	if (filename.match(/\/widget\d\.js/g)) {		//This IF statement is used to avoid type errors, not execute
-		regextest = filename.match(/\/widget\d\.js/g)[0];
-	}
 	
 	switch (filename) {
-		case regextest:								//If the server detects a request for a widget...
-			var matchtest = filename.match(/\d(?=\.js)/g)[0];
-			createWidget.example(res, matchtest);	//...then a function is called to serve the code for that widget
+		case '/widget.js':								//If the server detects a request for a widget...
+			createWidget.example(res);					//...then a function is called to serve the code for that widget
 		break;
 		case '/':
-			var filename = config.homepage;			//the default page is chosen from the configuration file
+			var filename = config.homepage;				//the default page is chosen from the configuration file
 		break;
 		default:
-			var filename = q.pathname;				//or, a specified page is chosen instead
+			var filename = q.pathname;					//or, a specified page is chosen instead
 		break;
 	}
 
 	fs.readFile("." + filename, (err, data) => {
-		if (err) {								//If the specified page isn't available, an error is created
+		if (err) {										//If the specified page isn't available, an error is created
 			res.writeHead(404, {'Content-Type': 'text/html'});
 			return res.end("404 Not Found");
 		} 
-		res.end(data);							//The page is given to the user
+		res.end(data);									//The page is given to the user
 	});
 }
 exports.createPage = createPage;
