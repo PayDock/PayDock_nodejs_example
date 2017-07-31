26517d659d099b5f7ec3311be1887a6257a4d0e5
Macbook Air <edward.bruin@paydock.com>
2017-07-31 13:18:57 +1000

    removed customer_id being sent through, it is now more secure as a (hard coded) server-side only variable server will process payments based on which fields are present and filled e.g vault_id or token cleaned up code a bit

--- paymentService.js
+++ paymentService.js
@@ -1,6 +1,9 @@
 var qs = require('querystring');
 var config = require('./config.json');
 var request = require('request');
+// TODO: customer id not sent tru its serverside only
+// TODO: choose which process to follow based on which fields are sent through
+// TODO: move sendcharge function call outside the switch
 
 function acceptPost (req, res) {
 	var incomingBody = '';
@@ -8,34 +11,35 @@ function acceptPost (req, res) {
 		incomingBody += data;							//the incoming message is processed for raw data
 	});
 	req.on('end', function(){
+		res.write('data received \n');
 		var parsedBody = JSON.parse(incomingBody);		//the data is then parsed into ready information
+		console.log(parsedBody);
 
-		switch (parsedBody.payment_type){
-			case "vault":
-				var outgoingBody = {							//the relevant information is grabbed from the message
-					"amount": parsedBody.amount,
-					"currency": parsedBody.currency,
-					"customer_id": parsedBody.customer_id,
-					"payment_source_id": parsedBody.token
-				}
-				console.log('payment type is vault');			
-				sendCharge(outgoingBody);
-				break;
-			case "token":
-				var outgoingBody = {							//the relevant information is grabbed from the message
+		var outgoingBody = {};
+
+		if (parsedBody.vault_id != null && parsedBody.vault_id != '') {
+			var outgoingBody = {							//the relevant information is grabbed from the message
+				"amount": parsedBody.amount,
+				"currency": parsedBody.currency,
+				"customer_id": config.customer_id,
+				"payment_source_id": parsedBody.token
+			}
+		} else if (parsedBody.token != null && parsedBody.token != ''){
+			var outgoingBody = {							//the relevant information is grabbed from the message
 					"amount": parsedBody.amount,
 					"currency": parsedBody.currency,
 					"token": parsedBody.token
 				}
-				console.log('payment type is token');			
-				sendCharge(outgoingBody);
-				break;
-			default:
-				console.log("error in payment type");
-				break;
+		} else {
+			console.log("payment type could not be identified");
+			res.write('payment type could not be identified \n');
+			res.end();
+			return;
 		}
 
-		res.write('data received \n');
+		sendCharge(outgoingBody);
+
+		res.write('payment has been processed \n');
 		res.end();
 	});
 }
