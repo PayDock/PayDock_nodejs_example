37f7d9d2243f9da51d56ec2859d2701ef9a2c5fe
Edward Bruin <edward.bruin@paydock.com>
2017-08-01 14:44:07 +1000

    no message

--- paymentService.js
+++ paymentService.js
@@ -36,35 +36,35 @@ function acceptPost (req, res) {
 			}
 		}
 		//console.log(outgoingBody);
-		sendCharge(outgoingBody);
+		sendCharge(outgoingBody, res);
 	});
+}
 
-	function sendCharge(outgoingBody){    	  						//the destination and content for a new message is given to this module
-		var SecretKey = config.SecretKey;	
-		var target = config.target;									//the destination and authentication for a new message are loaded from the configuration file
-		debugToConsole("sendCharge called");
-		request({                                  					//a new message is initialised                
-			url: target,
-			method: 'POST',
-			body: JSON.stringify(outgoingBody),
-			headers: {
-		      	'x-user-secret-key': SecretKey
-		  	}
-		}, function(error, response, body){      					//once the message is sent, the response is displayed
-			if(error) {
-				debugToConsole(error);
-			} else {
-				debugToConsole(response.statusCode + ', ' + body);
-				return endResponse(response.statusCode);
-			}
-		});
-	}
+function sendCharge(outgoingBody, callback){    	  						//the destination and content for a new message is given to this module
+	var SecretKey = config.SecretKey;	
+	var target = config.target;									//the destination and authentication for a new message are loaded from the configuration file
+	debugToConsole("sendCharge called");
+	request({                                  					//a new message is initialised                
+		url: target,
+		method: 'POST',
+		body: JSON.stringify(outgoingBody),
+		headers: {
+	      	'x-user-secret-key': SecretKey
+	  	}
+	}, function(error, response, body){      					//once the message is sent, the response is displayed
+		if(error) {
+			debugToConsole(error);
+		} else {
+			debugToConsole(response.statusCode + ', ' + body);
+			callback(response.statusCode, res);
+		}
+	});
+}
 
-	function endResponse (messageStatusCode) {
-		debugToConsole(messageStatusCode);
-		res.writeHead(messageStatusCode, {'Content-Type': 'application/json'});
-		res.end();
-	}
+function endResponse (messageStatusCode, res) {
+	debugToConsole(messageStatusCode);
+	res.writeHead(messageStatusCode, {'Content-Type': 'application/json'});
+	res.end();
 }
 
 function acceptVault(req, res) {
